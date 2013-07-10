bc93d70bf92743d0c3f9e5fc9a0023f78890d067
Sam Xiao <samxiaotj@gmail.com>
2013-07-10 18:15:46 +0800

    更新数据库

--- data/project_management.sql
+++ data/project_management.sql
@@ -10,15 +10,15 @@ Target Server Type    : MYSQL
 Target Server Version : 50612
 File Encoding         : 65001
 
-Date: 2013-07-06 16:50:25
+Date: 2013-07-10 18:15:29
 */
 
 SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
--- Table structure for `core_user`
+-- Table structure for `core_member`
 -- ----------------------------
-DROP TABLE IF EXISTS `core_user`;
-CREATE TABLE `core_user` (
+DROP TABLE IF EXISTS `core_member`;
+CREATE TABLE `core_member` (
   `id` int(11) NOT NULL,
   `username` varchar(64) NOT NULL,
   `password` varchar(32) NOT NULL,
@@ -28,7 +28,24 @@ CREATE TABLE `core_user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of core_user
+-- Records of core_member
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `pm_backlog`
+-- ----------------------------
+DROP TABLE IF EXISTS `pm_backlog`;
+CREATE TABLE `pm_backlog` (
+  `id` int(11) NOT NULL,
+  `project_id` int(11) NOT NULL,
+  `sprint_id` int(11) NOT NULL,
+  `title` varchar(64) NOT NULL,
+  `description` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of pm_backlog
 -- ----------------------------
 
 -- ----------------------------
@@ -46,6 +63,53 @@ CREATE TABLE `pm_project` (
 -- ----------------------------
 
 -- ----------------------------
+-- Table structure for `pm_project_member`
+-- ----------------------------
+DROP TABLE IF EXISTS `pm_project_member`;
+CREATE TABLE `pm_project_member` (
+  `member_id` int(11) NOT NULL,
+  `project_id` int(11) NOT NULL,
+  `time_on_project` int(11) NOT NULL COMMENT '在项目上的有效时间',
+  PRIMARY KEY (`member_id`,`project_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of pm_project_member
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `pm_sprint`
+-- ----------------------------
+DROP TABLE IF EXISTS `pm_sprint`;
+CREATE TABLE `pm_sprint` (
+  `id` int(11) NOT NULL,
+  `name` varchar(64) NOT NULL,
+  `start_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `end_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `project_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of pm_sprint
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `pm_status`
+-- ----------------------------
+DROP TABLE IF EXISTS `pm_status`;
+CREATE TABLE `pm_status` (
+  `id` int(11) NOT NULL,
+  `type` enum('') NOT NULL,
+  `value` varchar(64) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of pm_status
+-- ----------------------------
+
+-- ----------------------------
 -- Table structure for `pm_task`
 -- ----------------------------
 DROP TABLE IF EXISTS `pm_task`;
@@ -53,8 +117,9 @@ CREATE TABLE `pm_task` (
   `id` int(11) NOT NULL,
   `title` varchar(256) NOT NULL,
   `summary` text NOT NULL,
-  `project_id` int(11) NOT NULL,
+  `sprint_id` int(11) NOT NULL,
   `type` int(11) NOT NULL,
+  `assign_member_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
